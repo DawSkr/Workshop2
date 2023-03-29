a194e48d6da630271b7e7cfa3c06e869ea09cc1b
DawSkr <dawidskrobisz88@gmail.com>
2023-03-29 21:09:25 +0200

    Czesc pierwsza

--- Workshop2/src/main/java/pl/coderslab/Main.java
+++ Workshop2/src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,22 @@
 package pl.coderslab;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User();
+        UserDao userDao = new UserDao();
+
+//        user.setUserName("Krzysztof");
+//        user.setEmail("onet@onet.pl");
+//        user.setPassword("haslo123");
+
+//        userDao.create(user);
+        User user1 = userDao.read(1);
+
+      //  System.out.println(user1.getId() + " " + user1.getUserName() + user1.getEmail() + user1.getPassword());
+
+
     }
 }
